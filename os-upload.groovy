565840bffe2193c7384cc5e30d4054ed0a700cc0
YulianSalo <julian_salo@yahoo.com>
2021-04-25 23:48:20 +0300

    unzip script works

--- os-upload.groovy
+++ os-upload.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 def os_release=release="https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip"
+def reg_pattern=/^[0-9][0-9][0-9][0-9]-0-9][0-9].*zip$/
 pipeline {
     agent any 
     stages {
@@ -7,9 +8,9 @@ pipeline {
             steps {
                 // sh "wget ${release}"
                 sh """
-                mv ^[0-9][0-9][0-9][0-9]-0-9][0-9].*zip${5} raspberry.zip
+                searched_file=\$(ls -R | grep ^[0-9][0-9][0-9][0-9]-[0-9][0-9].*zip\$)
+                mv \$searched_file raspberry.zip
                 unzip raspberry.zip
-                rm ^[0-9][0-9][0-9][0-9]-0-9][0-9].*zip$
                 """
             }
         }
