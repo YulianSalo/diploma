be27ffe5a1dae03e58875021761af448dbd6070e
Yulian Salo <julian_salo@yahoo.com>
2021-07-11 00:24:37 +0300

    jenkinsfile updated

--- os-upload.groovy
+++ os-upload.groovy
@@ -1,70 +1,59 @@
 #!/usr/bin/env groovy
-def os_release=release="https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip"
-
 pipeline {
-    agent any
     
-    environment {
-    // Using returnStdout
-    CC = """${sh(
-            returnStdout: true,
-            script: 'echo "clang"'
-        )}""" 
-    // Using returnStatus
-    EXIT_STATUS = """${sh(
-            returnStatus: true,
-            script: 'exit 1'
-        )}"""
-    }
+    agent any
 
     stages {
-        stage('Download OS Release') { 
-            steps {
-                // sh "wget ${release}"
-                sh """
-                searched_file=\$(ls -R | grep ^[0-9][0-9][0-9][0-9]-[0-9][0-9].*zip\$)
-                mv \$searched_file raspberry_os_lite.zip
-                unzip raspberry.zip
-                """
+    
+        stage("script"){
+            stages {
+                stage('Prepare OS Release 1') {
+                    
+                    steps {
+                        script {
+                            // os_release = echo"\$(echo \$(python3 link_parser.py rpi_os_lite))"
+                            FILE_CONTENT = readFile("rpi_os_lite_rel.txt")
+                        }
+                    }
+                } 
+
+                stage('Prepare OS Release 2') {
+                    
+                    steps {
+                        script {
+                            OS_RELEASE = sh( script: "sudo python3 link_parser.py rpi_os_lite", , returnStdout: true).trim()
+                            sh "echo ${OS_RELEASE}"
+                        }
+                    }
+                        //file_content = readFile("rpi_os_lite_rel.txt")
+                } 
+
+                stage('Prepare OS Release 3'){
+
+                    steps {
+                    
+                        script{
+
+                            if (OS_RELEASE  == FILE_CONTENT) {
+                                
+                                echo "Done. Nothing new"
+
+                            } else {
+                                sh """
+                                wget ${OS_RELEASE}
+                                searched_file=\$(ls -R | grep ^[0-9][0-9][0-9][0-9]-[0-9][0-9].*zip\$)
+                                unzip \$searched_file
+                                os_var=\$(echo \$searched_file | rev | cut -c5- | rev)
+                                qemu-img convert -f raw "\$os_var.img" -O vmdk "\$os_var.vmdk"
+                                sshpass -f "psswd/vmware_psswd" scp "\$os_var.vmdk" root@192.168.0.100:/vmfs/volumes/5f4e581f-ffea726b-37a8-dca632e64c14/"\$os_var.vmdk"
+                                """
+                                echo "Done. Nothing new"
+                                
+                            }
+                        }   
+                    } 
+                }
             }
         }
-
-        stage('Prepare OS Image') { 
-            steps {
-                sh """
-                unzip raspberry.zip
-                qemu-img convert -f raw raspberry.img -O vmdk raspberry.vmdk
-                """
-            }
-        }
-
-        // stage('Copy OS Image To VMWare ESXi Host') { 
-        //     steps {
-        //         // sh """
-        //         // unzip raspberry.zip
-        //         // qemu-img convert -f raw raspberry.img -O vmdk raspberry.vmdk
-        //         // """
-        //     }
-        // }
-
-        // stage('Provision OS Image On VMWare ESXi Host') { 
-        //     steps {
-        //         //vmkfstools -i 2020-05-27-raspios-buster-arm64.vmdk -d thin raspios-buster-arm64.vmdk
-        //         // sh """
-        //         // unzip raspberry.zip
-        //         // qemu-img convert -f raw raspberry.img -O vmdk raspberry.vmdk
-        //         // """
-        //     }
-        // }
-
-        // stage('Place OS Image On VMWare ISCI Storage') { 
-        //     steps {
-        //         //vmkfstools -i 2020-05-27-raspios-buster-arm64.vmdk -d thin raspios-buster-arm64.vmdk
-        //         // sh """
-        //         // unzip raspberry.zip
-        //         // qemu-img convert -f raw raspberry.img -O vmdk raspberry.vmdk
-        //         // """
-        //     }
-        // }
     }
 }
